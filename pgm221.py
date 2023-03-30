1f4eab5182343f94d97d1a5d4f7116845b6e868b
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 08:06:51 +0530

    fc

--- pgm221.py
+++
@@ -1,8 +0,0 @@
-# extract the unique value of the list
-import random
-n=int(input('n=?'))
-a=[random.randint(0,100)for i in range(n)]
-print("list a:",a)
-b=set(a)
-print("length of set b",len(b))
-print("set b",b)
