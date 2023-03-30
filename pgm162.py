36320d270b9c70ff26eb88a497d5de0f334e0041
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 07:00:59 +0530

    wg

--- pgm162.py
+++
@@ -1,7 +0,0 @@
-# Read the string in reverse order using for loop
-n=input('n=?')
-l=len(n)
-print("Read the string in reverse order using for loop")
-for i in range(l-1,-1,-1):
-    print(n[i],end='')
-print()
