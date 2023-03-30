6d692b4af9347b57fb46964793f13e76ddd359f2
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 07:15:26 +0530

    wg

--- copy n character from the end.py
+++
@@ -1,9 +0,0 @@
-# copy n character from the end
-s1=input('s1=?')
-m=int(input('m=?'))
-n=int(input('n=?'))
-s2=''
-k=m+n
-for i in range(-m,-k,-1):
-    s2=s2+s1[i]
-print("copy n character from the end in a string ",s2)
