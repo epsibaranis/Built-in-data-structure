b7ae0a97430c5229e782af1ee6594be97005c01a
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 08:01:01 +0530

    Delete pgm214.py

--- pgm214.py
+++
@@ -1,8 +0,0 @@
-# tuple of n elements print it element by element
-import random
-n=int(input('n=?'))
-a=(random.randint(0,100) for j in range(n))
-print("Tuple",a)
-print("tuple of n elements print it element by element")
-for i in a:
-    print(i)
